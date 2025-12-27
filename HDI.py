88f561e61365aabc2160685250694779cd9dd89b
Vladislaus-37 <axi.studios@yandex.ru>
2025-12-27 09:28:40 +0300

    Update HDI.py
    Sawed into procedures

--- HDI.py
+++ HDI.py
@@ -9,80 +9,159 @@ import sys
 import colorama
 colorama.init()
 
-
 # Read config file
 with open("config.conf", 'r', encoding='utf-8') as file:
     configdata = file.readlines()
 file.close()
 
+def main():
+    return 0
+
+def index():    # Index command
+    try:
+        print(' ')
+        print('Contents of', os.getcwd()) 
+        print('------------------------------------------------------------------')
+        print('| Type  | Name            | Size                                 |')
+        print('|-------|-----------------|--------------------------------------|')
+        for item in os.listdir():
+            if os.path.isfile(item):
+                print('| File: | ' + item + ' | ' + str(os.path.getsize(item)) + ' bytes |')
+            if os.path.isdir(item):
+                print('| Dir:  | ' + item + ' | ' + str(os.path.getsize(item)) + ' bytes |')
+        print('------------------------------------------------------------------')
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    INDEX_ERR')
+        print(colorama.Fore.WHITE + '    May be path of dir is invalid')
+        print('    Or your system is do not supported')
+    return 0
+
+def read():     # Read txt file
+    try:
+        print('Dir:',os.getcwd())
+        with open(input('File:'), 'r') as file:
+            contenta = file.read()
+        print(colorama.Fore.LIGHTBLACK_EX+'------------------')
+        print(colorama.Fore.LIGHTBLUE_EX+' ')
+        print(contenta)
+        print(colorama.Fore.LIGHTBLUE_EX+' ')
+        print(colorama.Fore.LIGHTBLACK_EX+'------------------')
+        print(colorama.Fore.WHITE+'')
+        file.close()
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    READ_ERR')
+        print(colorama.Fore.WHITE + '    May be name of file is invalid')
+        print('    Or your system is do not supported')
+    return 0
+
+def runfile():       # Run file
+    try:
+        print('Dir:',os.getcwd())
+        filepath = os.getcwd() + "/" + input('File:')
+        if platform.system() == 'Darwin':       # for macOS
+            subprocess.call(('open', filepath))
+        elif platform.system() == 'Windows':    # for Windows
+            os.startfile(filepath)
+        else:                                   # for Pinguins
+            subprocess.call(('xdg-open', filepath))
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    OPEN_ERR')
+        print(colorama.Fore.WHITE + '    May be name of file is invalid')
+        print('    Or your system is do not supported')
+    return 0
+
+def chdir():        # Change dir
+    try:
+         os.chdir(input('Dir:'))
+         print('Dir will be changed!')
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    CHDIR_ERR')
+        print(colorama.Fore.WHITE + '    May be path of dir is invalid')
+        print('    Or your system is do not supported')
+    return 0
+
+def delfile():       # Delete flie
+    try:
+        print('Dir:',os.getcwd())
+        os.remove(input('File:'))
+        print('File will be deleted!')
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    DEL_FILE_ERR')
+        print(colorama.Fore.WHITE + '    May be path of file is invalid')
+        print('    Or your system is do not supported')
+    return 0
+
+def deldir():       # Delete dir
+    dirpath=input('Dir:')
+    try:
+        os.rmdir(dirpath)
+        print('Dir will be deleted')
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    DEL_DIR_ERR')
+        print(colorama.Fore.WHITE + '    May be name of dir is invalid')
+        print('    Or your dir is not empty')
+        print('    Or your system is do not supported')
+    return 0
+
+def rename():
+    try:
+        print('Current dir:',os.getcwd())
+        os.rename(input('File or path of dir:'), input('Rename to:'))
+        print('File or dir will be renamed!')
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    RENAME_ERR')
+        print(colorama.Fore.WHITE + '    May be path of file is invalid')
+        print('    Or your system is do not supported')
+    return 0
+
+def cdir():     # Create dir
+    try:
+        os.mkdir(input('Path of new dir:'))
+        print('Dir will be created!')
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    CREATE_DIR_ERR')
+        print(colorama.Fore.WHITE + '    May be path of new dir is invalid')
+        print('    Or your system is do not supported')
+    return 0
+
+def f_ck_copy():     # Copy file
+    src = input('Source path:')
+    dest = input('Copy to:')
+    print(src, dest) 
+    try:
+        if platform.system() == 'Darwin':       # for macOS
+            subprocess.call(('cp', src, dest))
+        elif platform.system() == 'Windows':    # for Windows
+            subprocess.call(('cmd', '/c', 'copy', src, dest))
+        else:                                   # for Pinguins
+            subprocess.call(('cp', src, dest))
+    except:
+        print(colorama.Fore.LIGHTRED_EX + '    COPY_ERR')
+        print(colorama.Fore.WHITE + '    May be name of file is invalid')
+        print('    Or your system is do not supported')
+    return 0
 
 # Output Header
 print(colorama.Fore.WHITE+colorama.Back.BLACK+' ')
-print('HDI β6')
+print('HDI β7')
 print(colorama.Style.RESET_ALL+'—————————')
 
 
 # Main part
 while True:
     cow=input('$').strip() 
+
     if cow=='index': # Index command
-        try:
-            print(' ')
-            print('Contents of', os.getcwd())
-            print('------------------------------------------------------------------')
-            print('| Type  | Name            | Size                                 |')
-            print('|-------|-----------------|--------------------------------------|')
-            for item in os.listdir():
-                if os.path.isfile(item):
-                    print('| File: | ' + item + ' | ' + str(os.path.getsize(item)) + ' bytes |')
-                if os.path.isdir(item):
-                   print('| Dir:  | ' + item + ' | ' + str(os.path.getsize(item)) + ' bytes |')
-            print('------------------------------------------------------------------')
-        except:
-            print(colorama.Fore.LIGHTRED_EX + '    INDEX_ERR')
-            print(colorama.Fore.WHITE + '    May be path of dir is invalid')
-            print('    Or your system is do not supported')
+        index()
 
     elif cow=='read': # Read txt file
-        try:
-            print('Dir:',os.getcwd())
-            with open(input('File:'), 'r') as file:
-                contenta = file.read()
-            print(colorama.Fore.LIGHTBLACK_EX+'------------------')
-            print(colorama.Fore.LIGHTBLUE_EX+' ')
-            print(contenta)
-            print(colorama.Fore.LIGHTBLUE_EX+' ')
-            print(colorama.Fore.LIGHTBLACK_EX+'------------------')
-            print(colorama.Fore.WHITE+'')
-            file.close()
-        except:
-            print(colorama.Fore.LIGHTRED_EX + '    READ_ERR')
-            print(colorama.Fore.WHITE + '    May be name of file is invalid')
-            print('    Or your system is do not supported')
+        read()
 
     elif cow=='open': # Open file with default application
-        try:
-            print('Dir:',os.getcwd())
-            filepath = os.getcwd() + "/" + input('File:')
-            if platform.system() == 'Darwin':       # for macOS
-                subprocess.call(('open', filepath))
-            elif platform.system() == 'Windows':    # for Windows
-                os.startfile(filepath)
-            else:                                   # for Pinguins
-                subprocess.call(('xdg-open', filepath))
-        except:
-            print(colorama.Fore.LIGHTRED_EX + '    OPEN_ERR')
-            print(colorama.Fore.WHITE + '    May be name of file is invalid')
-            print('    Or your system is do not supported')
+        runfile()
 
     elif cow=='chdir': # Change dir
-         try:
-             os.chdir(input('Dir:'))
-             print('Dir will be changed!')
-         except:
-            print(colorama.Fore.LIGHTRED_EX + '    CHDIR_ERR')
-            print(colorama.Fore.WHITE + '    May be path of dir is invalid')
-            print('    Or your system is do not supported')
+        chdir()
 
     elif cow=='exit': # For haters)
         sys.exit()
@@ -106,63 +185,22 @@ while True:
         print('  about       - About HDI')
 
     elif cow=='del file' or cow=='delete file': # Delete file
-        try:
-            print('Dir:',os.getcwd())
-            os.remove(input('File:'))
-            print('File will be deleted!')
-        except:
-            print(colorama.Fore.LIGHTRED_EX + '    DEL_FILE_ERR')
-            print(colorama.Fore.WHITE + '    May be path of file is invalid')
-            print('    Or your system is do not supported')
+        delfile()
 
     elif cow=='del dir' or cow=='delete dir': # Delete dir
-        dirpath=input('Dir:')
-        try:
-            os.rmdir(dirpath)
-            print('Dir will be deleted')
-        except:
-            print(colorama.Fore.LIGHTRED_EX + '    DEL_DIR_ERR')
-            print(colorama.Fore.WHITE + '    May be name of dir is invalid')
-            print('    Or your dir is not empty')
-            print('    Or your system is do not supported')
+        deldir()
 
     elif cow=='rename' or cow=='renm': # Rename file or dir
-        try:
-            print('Current dir:',os.getcwd())
-            os.rename(input('File or path of dir:'), input('Rename to:'))
-            print('File or dir will be renamed!')
-        except:
-            print(colorama.Fore.LIGHTRED_EX + '    RENAME_ERR')
-            print(colorama.Fore.WHITE + '    May be path of file is invalid')
-            print('    Or your system is do not supported')
+        rename()
 
     elif cow=='create dir' or cow=='cdir': # Create dir
-        try:
-            os.mkdir(input('Path of new dir:'))
-            print('Dir will be created!')
-        except:
-            print(colorama.Fore.LIGHTRED_EX + '    CREATE_DIR_ERR')
-            print(colorama.Fore.WHITE + '    May be path of new dir is invalid')
-            print('    Or your system is do not supported')
+        cdir()
 
     elif cow=='cudir': # Show current dir
         print('Current dir:   ',os.getcwd())
 
     elif cow=='copy': # Copy file
-        src = input('Source path:')
-        dest = input('Copy to:')
-        print(src, dest) 
-        try:
-            if platform.system() == 'Darwin':       # for macOS
-                subprocess.call(('cp', src, dest))
-            elif platform.system() == 'Windows':    # for Windows
-                subprocess.call(('cmd', '/c', 'copy', src, dest))
-            else:                                   # for Pinguins
-                subprocess.call(('cp', src, dest))
-        except:
-            print(colorama.Fore.LIGHTRED_EX + '    COPY_ERR')
-            print(colorama.Fore.WHITE + '    May be name of file is invalid')
-            print('    Or your system is do not supported')
+        f_ck_copy()
     
     elif cow=='qr': # Emm, here was be a qr code to NOWKIES Telegram post
         print('Ha!')
@@ -173,7 +211,7 @@ while True:
         print('▓▓▓▓▓▓▓▓▓▓▓▓▒')
         print('▓           ▒       ',colorama.Back.LIGHTWHITE_EX+colorama.Fore.BLACK+'αxi')
         print(colorama.Back.BLACK+colorama.Fore.WHITE+'▓ HDI       ▒')
-        print('▓ ────      ▒',colorama.Style.BRIGHT+colorama.Fore.LIGHTWHITE_EX+'       HDI β6')
+        print('▓ ────      ▒',colorama.Style.BRIGHT+colorama.Fore.LIGHTWHITE_EX+'       HDI 1.0')
         print(colorama.Style.RESET_ALL+colorama.Fore.WHITE+'▓ $         ▒')
         print('▓           ▒')
         print('▒▒▒▒▒▒▒▒▒▒▒▒▒')
